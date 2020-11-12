e8d8c375c9ca6202e69422ebea46d1606dda6559
Pinyk <1748708966@qq.com>
2020-11-12 10:38:15 +0800

    del pdf

--- 数据处理/DemoOne.py
+++ 数据处理/DemoOne.py
@@ -44,3 +44,43 @@ plt.show()
 # ax = df1.plot.scatter(x='产品名称',y=y,color='DarkBlue',label='Class 1')
 # df.plot.scatter(x='客户等级',y=y,color='DarkGreen',label='Class 2',ax=ax)
 # plt.show()
+
+# 网络类型各占的比例
+typeOne = data['网络类型']
+arrayOne = {}
+set_net = set(typeOne)
+print(set_net)
+for i in set_net:
+    arrayOne[i] = 0
+for i in typeOne:
+    arrayOne[i] = arrayOne[i]+1
+print(arrayOne)
+labelOne = []
+numberOne = []
+for i in set_net:
+    labelOne.append(i)
+    numberOne.append(arrayOne[i])
+explode = [0.1, 0.1, 0.1]
+plt.pie(numberOne, labels=labelOne, explode=explode, autopct='%1.2f%%')
+plt.show()
+
+
+
+# 用户类型各占的比例
+type = data['用户类型']
+array = {}
+set_People = set(type)
+print(set_People)
+for i in set_People:
+    array[i] = 0
+for i in type:
+    array[i] = array[i]+1
+print(array)
+label = []
+number = []
+for i in set_People:
+    label.append(i)
+    number.append(array[i])
+explode = [0.1, 0.1]
+plt.pie(number, labels=label, explode=explode, autopct='%1.2f%%')
+plt.show()
